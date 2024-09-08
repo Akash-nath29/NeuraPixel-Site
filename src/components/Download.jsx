07226a10aab6802235abbcd435689fae39b62f7c
Akash-nath29 <anath5440@gmail.com>
2024-09-08 22:08:02 +0530

    Fixed Download Issue

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,22 +1,14 @@
 import './css/Download.css';
 
 function Download() {
-  const handleAndroidDownload = () => {
-    const link = document.createElement('a');
-    link.href = './assets/neurapixelai.apk'; // Adjust the path if necessary
-    link.download = 'neurapixelai.apk';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
 
   return (
     <div className="download">
       <h1>Download</h1>
       <p>Get our powerful MRI denoising tool for your desktop.</p>
       <div className="download-options">
-        <button className="download-button" onClick={handleAndroidDownload}>Download for Android</button>
-        <button className="download-button" onClick={alert("Coming Soon")}>Download for iOS</button>
+        <button className="download-button"><a href="https://neurapixel.vercel.app/assets/neurapixelai.apk">Download for Android</a></button>
+        <button className="download-button" onClick={() => alert("Coming Soon")}>Download for iOS</button>
       </div>
       <div className="app-features">
         <h2>Key Features</h2>
