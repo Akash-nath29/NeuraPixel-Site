f38b61a20aa04ecf1cee4ab1d56191d59db05129
Akash-nath29 <anath5440@gmail.com>
2024-09-08 17:24:07 +0530

    Fixed Contact Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,17 +6,18 @@ function Contact() {
     <div className="contact">
       <h1>Contact Us</h1>
       <p>Have questions or need support? Reach out to us!</p>
-      <form action="https://formsubmit.co/devakash2905@gmail.com" method="POST">
-        {/* <!-- HoneyPot --> */}
-        <input type="text" name="_honey" style="display: none;" />
+      <form action="https://formsubmit.co/devakash2905@gmail.com" method="POST" id="contact-form">
+        {/* HoneyPot */}
+        <input type="text" name="_honey" style={{display: 'none'}} />
 
-        {/* <!-- Disable captcha --> */}
+        {/* Disable captcha */}
         <input type="hidden" name="_captcha" value="false" />
         <div className="form-group">
           <label htmlFor="name">Name:</label>
           <input
             type="text"
             id="name"
+            name="name"
             required
           />
         </div>
@@ -25,6 +26,7 @@ function Contact() {
           <input
             type="email"
             id="email"
+            name="email"
             required
           />
         </div>
@@ -32,6 +34,7 @@ function Contact() {
           <label htmlFor="message">Message:</label>
           <textarea
             id="message"
+            name="message"
             required
           ></textarea>
         </div>
