961afdd3a4e5803cf4956eedb2c29087c8fae3c6
Akash-nath29 <anath5440@gmail.com>
2024-09-08 22:42:10 +0530

    Update Download Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './css/Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2024 NeuraPixel AI. All rights reserved.</p>
+      <p>&copy; 2024 <a href="https://neurapixel.vercel.app/">NeuraPixel AI</a>. All rights reserved.</p>
     </footer>
   );
 }
